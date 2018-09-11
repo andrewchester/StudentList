387e75022b87b0ae3a2d9dfa087a419961408b07
U-BSD\476724 <476724@SHS-10L74C39E.beaverton.k12.or.us>
2018-09-11 09:13:29 -0700

    Added createStudent and get's the user's command

--- main.cpp
+++ main.cpp
@@ -10,9 +10,9 @@ struct Student
   double gpa;
 };
 
-void createStudent()
+void createStudent(std::vector<Student>* students)
 {
-  
+  (*students).
 }
 
 int main()
@@ -22,23 +22,32 @@ int main()
 
   char command[5];
   
-  std::vector<int>* students;
+  std::vector<Student>* students;
   
   while(running)
   {
     std::cout << "Enter a command(ADD, PRINT, DELETE, QUIT): ";
     std::cin.get(command, 5);
     std::cin.clear();
-    std::cin.ignore(100, '/n');
-
-    for(int i = 0; i < strlen(command); i++)
+    std::cin.ignore(100, '\n');
+    
+    for(int i = 0; i < sizeof(command); i++)
     {
       int asciiValue = (int)command[i];
       if(asciiValue >= 97 && asciiValue <= 122)
 	command[i] = (char)(asciiValue - 32);
     }
+
+    if(strncmp(command, "QUIT", 5) == 0)
+    {
+      std::cout << "Exiting";
+      break;
+    }
+    else if(strncmp(command, "ADD", 5) == 0)
+    {
+      createStudent();
+    }
   }
-      
-  
+    
   return 0;
 }
